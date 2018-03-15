38721f4f0e93b55e0f4f8e1ba6b032c280579fd6
Hack Reactor Student <student@hackreactor.com>
2018-03-15 12:31:00 -0700

    added routes

--- server/routes/navbar.js
+++ server/routes/navbar.js
@@ -4,9 +4,13 @@ const router = express.Router();
 router.get('/business/:name', (req, res) => {
 	let name = req.params.name;
 
-	
+
 
 	res.send('hello ' + name);
+});
+
+router.get('/city/:name', (req, res) => {
+
 });
 
 module.exports = router;
