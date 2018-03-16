80e0b6e45ca4f97575b0f44a486c2b6cd79a3bd7
fermi_dirac <ManueleBryan@gmail.com>
2018-03-15 21:06:23 -0700

    wrote slow worker

--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const stream = require('stream');
+const faker = require('faker');
 
 const connection = mysql.createConnection({
 	host: 'localhost',
@@ -8,25 +9,90 @@ const connection = mysql.createConnection({
 	password: '',
 });
 
-connection.connect(function(err) {
-	if (err) {
-		console.error(err);
-	} else {
-		connection.query('SELECT city FROM business')
-			.stream()
-			.pipe(stream.Transform({
-				objectMode: true,
-				transform: (data, encoding, callback) => {
+var recursivelySeed = (connection) => {
+	connection.query('SELECT COUNT(*) as count FROM business', (err, data) => {
+		var businessSize = data[0].count;
 
-					connection.query(`INSERT INTO city (name) VALUES ('${data.city}')`, (err, result) => {
-						if (err) {
-							console.error(err);
-						}
-					});
+		if (businessSize > 10000000) {
+			connection.end();
+			return;
+		}
 
-					callback();
-				}
-			}))
-			.on('finish', () => { console.log('finished')})
-	}
-});
+		console.log('businessSize:', businessSize);
+
+		var values = [];
+
+		for (let i = 0; i < 10000; ++i) {
+			var id = mysql.escape(faker.random.alphaNumeric(16));
+			var name = mysql.escape(faker.company.companyName());
+			var neighborhood = mysql.escape(faker.address.county());
+			var address = mysql.escape(faker.address.streetAddress());
+			var city = mysql.escape(faker.address.city());
+			var state = mysql.escape(faker.address.stateAbbr());
+			var postal_code = mysql.escape(faker.address.zipCode());
+			var longitude = mysql.escape(parseFloat(faker.address.longitude()));
+			var latitude = mysql.escape(parseFloat(faker.address.latitude()));
+			var stars = mysql.escape(faker.random.number(5));
+			var review_count = mysql.escape(faker.random.number(64));
+			var is_open = mysql.escape(faker.random.number(1));
+
+			values.push(`(${id}, ${name}, ${neighborhood}, ${address}, ${city}, ${state}, ${postal_code}, ${latitude}, ${longitude}, ${stars}, ${review_count}, ${is_open})`);
+		}
+
+		values = values.join(', ');
+
+		connection.query(`INSERT INTO business (id, name, neighborhood, address, city, state, postal_code, latitude, longitude, stars, review_count, is_open) VALUES ${values}`, (err, data) => {
+			if (err) console.error('fail');
+		});
+
+		recursivelySeed(connection);
+
+	});
+
+}
+
+connection.connect();
+
+recursivelySeed(connection);
+
+
+
+
+// .then((connection) => {
+// 	connection.query('SELECT COUNT(*) AS count FROM business')
+// 		.then((data) => {
+// 			return data[0].count;
+// 		})
+// 		.then((count) => {
+// 			console.log(count);
+
+// 			for (let i = count; i < 10000000; ++i) {
+// 				var name = faker.company.companyName();
+// 				var neighborhood = faker.address.county();
+// 				var address = faker.address.streetAddress();
+// 				var city = faker.address.city();
+// 				var state = faker.address.stateAbbr();
+// 				var postal_code = faker.address.zipCode();
+// 				var longitude = faker.address.longitude();
+// 				var latitude = faker.address.latitude();
+// 				var stars = faker.random.number(5);
+// 				var review_count = faker.random.number(64);
+// 				var is_open = faker.random.number(1);
+
+// 				connection.query(`INSERT INTO business
+// 					(name, neighborhood, address, city, state, postal_code, latitude, longitude, stars, review_count, is_open)
+// 					VALUES (${name}, ${neighborhood}, ${address}, ${city}, ${state}, ${postal_code}, ${latitude}, ${longitude}, ${stars}, ${review_count}, ${is_open})
+// 				`).then(() => {
+// 					console.log('hi');
+// 				})
+// 				.catch((err) => {
+// 					throw err;
+// 				})
+// 			}
+
+// 		});
+
+// 	return connection;
+// }).then((connection) => {
+// 	// connection.end();
+// });
